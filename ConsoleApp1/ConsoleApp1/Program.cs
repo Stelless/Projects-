7d37e403e10df103de4a8351cfd9d3096ee2e27d
Stelless <47225505+Stelless@users.noreply.github.com>
2019-01-31 11:25:57 -0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Matrix matTest = new Matrix(3, 3);
+            matTest.displayInfo();
+            matTest.initializeRows();
+            matTest.displayMatrix();
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Matrix
+    {
+        int rows;
+        int columns;
+
+        int[] rowVal;
+        int[] colVal;
+
+        int[,] matrix;
+
+        // CONSTRUCTOR
+        public Matrix (int _rows, int _columns)
+        {
+            rows = _rows;
+            columns = _columns;
+            rowVal = new int[_rows];
+            colVal = new int[_columns];
+            matrix = new int[_rows, _columns];
+
+
+        }
+
+        public void displayInfo()
+        {
+            Console.WriteLine("Rows: " + rows);
+            Console.WriteLine("Columns: " + columns);
+
+        }
+
+        public void initializeRows()
+        {
+
+            for (int i = 0; i < rows; i++)
+            {
+                int _i = i + 1;
+                Console.WriteLine("Row: " + _i + " With " + columns + " columns");
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        public void displayMatrix()
+        {
+            // Cycles through the rows one at a time entering all of the columns data at once then moving onto the next row.
+            for (int i = 0; i < rows; i++)
+            {
+
+                for (int j = 0; j < columns; j++)
+                {
+                    // Console.WriteLine(matrix[i,j]);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("End Function");
+
+        }
+    }
+}
+
+
